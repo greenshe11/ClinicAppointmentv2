3d421ee6a588ee13a7c248288743d410d09fa436
Eugene Dave Tumagan <eugenetumagan02@gmail.com>
2025-04-02 20:36:54 +0800

    mview symptom selector

--- utilities/test.py
+++ utilities/test.py
@@ -5,6 +5,8 @@ account_sid = 'AC3cbab85a43181a3e553a4d91792ddf8c'
 auth_token = 'da771263b8b8b3e609306fec37f3f879'
 client = Client(account_sid, auth_token)
 message = client.messages.create(
-    to='639674688324'
+    body='testing',
+    from_='+17087346648',
+    to='+639192248798'
 )
 print(message.sid)
